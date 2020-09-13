4962c1e16885d239401b14c8908b14cc6ae0dbdc
Bryan White <bwhite@eastcoastcatalyst.com>
2020-09-13 11:48:32 -0400

    Added more breakpoint for case story option images

---
+++ src/pages/leadership.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+import SEO from '../components/seo'
+
+const IndexPage = ({data}) => {
+  return (
+    <Layout>
+		<SEO title="Leadership - skylytics" description="Site description" />
+		<h1>HELLO WORLD</h1>
+    </Layout>
+  )
+}
+
+
+
+export default IndexPage
