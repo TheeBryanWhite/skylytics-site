c665a33fad8ba7e08ae3385603e03bfa9c6def4a
Bryan White <bwhite@eastcoastcatalyst.com>
2020-09-19 12:00:55 -0400

    Fix to lodash library

--- src/components/elevator-pitch/elevatorpitch.js
+++ src/components/elevator-pitch/elevatorpitch.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import BackgroundImage from 'gatsby-background-image'
+import LazyLoad from 'react-lazyload'
 
 import './elevatorpitch.scss'
 
 const ElevatorPitch = props => {
 	return (
 		<section className="elevator-pitch" id="about-us">
-			<BackgroundImage
-				className="bgnull"
-				fluid={props.aboutUsBody.background_image.localFile.childImageSharp.fluid}
-				style={{
-					backgroundPosition: 'top right'
-				}}
-				Tag="div"
-			>
-			</BackgroundImage>
-			<div className="epcopy">
-				<div className="container">
-					<div dangerouslySetInnerHTML={{ __html: props.aboutUsBody.about_us_body.html }} />
-					<p><a className="cta" href={props.aboutUsBody.body[0].primary.link.text}>{props.aboutUsBody.body[0].primary.title.text}</a></p>
+			<LazyLoad height={200}>
+				<BackgroundImage
+					className="bgnull"
+					fluid={props.aboutUsBody.background_image.localFile.childImageSharp.fluid}
+					style={{
+						backgroundPosition: 'top right'
+					}}
+					Tag="div"
+				>
+				</BackgroundImage>
+				<div className="epcopy">
+					<div className="container">
+						<div dangerouslySetInnerHTML={{ __html: props.aboutUsBody.about_us_body.html }} />
+						<p><a className="cta" href={props.aboutUsBody.body[0].primary.link.text}>{props.aboutUsBody.body[0].primary.title.text}</a></p>
+					</div>
 				</div>
-			</div>
+			</LazyLoad>
 		</section>
 	)
 }
