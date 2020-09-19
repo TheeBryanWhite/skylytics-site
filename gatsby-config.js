cd06d75cad2e335f2b65dee3fb75aef3c32e6677
Bryan White <bwhite@eastcoastcatalyst.com>
2020-09-19 01:08:31 -0400

    Added guess.js support

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,6 +118,24 @@ module.exports = {
     },
     // Uncomment when in production
     // {
+    //   resolve: "gatsby-plugin-guess-js",
+    //   options: {
+    //     // Find the view id in the GA admin in a section labeled "views"
+    //     GAViewID: `VIEW_ID`,
+    //     // Add a JWT to get data from GA
+    //     jwt: {
+    //       client_email: `GOOGLE_SERVICE_ACCOUNT_EMAIL`,
+    //       private_key: `GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY`,
+    //     },
+    //     minimumThreshold: 0.03,
+    //     // The "period" for fetching analytic data.
+    //     period: {
+    //       startDate: new Date("2018-1-1"),
+    //       endDate: new Date(),
+    //     },
+    //   },
+    // },
+    // {
     //   resolve: 'gatsby-plugin-google-tagmanager',
     //   options: {
     //     id: '',
