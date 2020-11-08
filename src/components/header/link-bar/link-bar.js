9d4506769481083266fe983706e4d6fddaf43297
Bryan White <bwhite@eastcoastcatalyst.com>
2020-11-07 20:34:17 -0500

    Updated header link bar link display

--- src/components/header/link-bar/link-bar.js
+++ src/components/header/link-bar/link-bar.js
@@ -6,7 +6,7 @@ const LinkBar = () => {
 	return(
 		<div className="link-bar">
 			<div className="container">
-				<p><a href="https://www.safercontact.com" target="_blank" rel="noreferrer">safercontact</a></p>
+				<p><a href="https://www.safercontact.com" target="_blank" rel="noreferrer">safercontact.com</a></p>
 			</div>
 		</div>
 	)	
