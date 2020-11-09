9e9d3e373ec75a82dec3089410fb9ab6f3d206a5
Mike Hickey <59627140+hickem@users.noreply.github.com>
2020-11-09 14:56:29 -0500

    Update link-bar.js
    Updated text, but this is not showing up on the page.

--- src/components/header/link-bar/link-bar.js
+++ src/components/header/link-bar/link-bar.js
@@ -6,7 +6,7 @@ const LinkBar = () => {
 	return(
 		<div className="link-bar">
 			<div className="container">
-				<p>Visit <a href="https://www.safercontact.com" target="_blank" rel="noreferrer">safercontact.com</a></p>
+				<p>visit <a href="https://www.safercontact.com" target="_blank" rel="noreferrer">safercontact.com</a></p>
 			</div>
 		</div>
 	)	
