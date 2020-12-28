352ac544d99e8db4e34f464b4990067f86960b0f
Bryan White <bwhite@eastcoastcatalyst.com>
2020-12-28 12:27:44 -0500

    News sorting update

--- src/templates/news.js
+++ src/templates/news.js
@@ -30,7 +30,7 @@ function NewsPostTemplate({data}) {
 }
 
 export const query = graphql`
-query PostBySlug {
+query PostBySlug($uid: String!) {
 	prismicPage(uid: {eq: "newsroom"}) {
 		id
 		data {
@@ -45,33 +45,23 @@ query PostBySlug {
 		  }
 		}
 	  }
-	  allPrismicNews(sort: {fields: [first_publication_date], order: DESC}) {
-		edges {
-		  node {
-			first_publication_date(formatString: "MMMM DD, YYYY")
-			last_publication_date
-			uid
-			data {
-			  news_body {
+	prismicNews(uid: {eq: $uid}) {
+		data {
+			news_body {
 				html
-				raw
-				text
-			  }
-			  news_category
-			  news_hero_image {
-				fluid {
-				  src
-				}
-			  }
-			  news_title {
-				html
-				raw
-				text
-			  }
 			}
-		  }
+			news_category
+			news_hero_image {
+				fluid {
+					src
+				}
+			}
+			news_title {
+				text
+			  }
 		}
-	  }
+		first_publication_date(formatString: "MMMM DD, YYYY")
+	}
 }
 `
 
